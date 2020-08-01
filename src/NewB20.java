a93c9c1632ed6523da62b9820d351d5aa90084a0
Yuksel Ozdemir <yuksel.ozdemir@msn.com>
2020-08-01 12:36:21 -0400

    add class newB20.class

--- src/NewB20.java
+++ src/NewB20.java
@@ -1,5 +1,7 @@
 public class NewB20 {
     public static void main(String[] args) {
         System.out.println("New Class");
+        // new lines
+        System.out.println("Adding lines");
     }
 }
