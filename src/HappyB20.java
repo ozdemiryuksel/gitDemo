37369cc7962ef9894abf18a9f50850788154267d
Yuksel Ozdemir <yuksel.ozdemir@msn.com>
2020-08-01 11:57:09 -0400

    add HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,6 @@ public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("B20 is always happy");
         System.out.println("test");
+        // new line added
     }
 }
